f37648f5fad777cba7f49ed3e8bc5d17bcc24825
Ferenc- <Ferenc-@users.noreply.github.com>
2019-11-24 01:44:56 +0100

    Use latest stable API version of pyftdi

--- installPyFtdi.sh
+++ installPyFtdi.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 # https://learn.adafruit.com/circuitpython-on-any-computer-with-ft232h/linux
-# source the virtualenv activation first
 
-pip3 install pyftdi adafruit-blinka
+# source the virtualenv activation first
+virtualenv /tmp/ft232h
+source /tmp/ft232h/bin/activate
+
+#Unfortunately the current latest v0.40 series seems to be broken
+#pip3 install pyftdi adafruit-blinka
+pip3 install https://codeload.github.com/eblot/pyftdi/tar.gz/v0.30.3 adafruit-blinka
+
 
 export BLINKA_FT232H=1
 python3 -c 'from pyftdi.ftdi import Ftdi
