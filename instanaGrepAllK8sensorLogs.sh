e215edd8ed83d920ff3e402c7be3a98a7b3db8a0
Ferenc Géczi <ferenc.gm@gmail.com>
2025-09-01 00:00:00 +0000

    Add instanaGrepAllK8sensorLogs.sh
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

---
+++ instanaGrepAllK8sensorLogs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+PATTERN="${1:-app=k8sensor@}"
+
+for po in $(kubectl get -n instana-agent po -l app=k8sensor -o name | cut -f2 -d '/'); do kubectl logs -n instana-agent "${po}" | grep "${PATTERN}" ; done
