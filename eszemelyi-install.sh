d6e81294c73adb57ff7df947f84fc02057e80217
Ferenc Géczi <ferenc.gm@gmail.com>
2025-09-20 00:00:00 +0000

    Add libnfc workarounds
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- eszemelyi-install.sh
+++ eszemelyi-install.sh
@@ -4,4 +4,10 @@
 wget 'https://eszemelyi.hu/wp-content/uploads/2023/05/eSzemelyi_Kliens_x64_1_7_3.deb'
 sudo apt install ./eSzemelyi_Kliens_x64_1_7_3.deb
 
+# Apply libnfc workarounds from 
+# https://github.com/nfc-tools/libnfc/issues/666#issuecomment-2061370787
+# and
+# https://github.com/nfc-tools/libnfc/issues/684#issuecomment-1632260381
+LD_PRELOAD=/usr/local/lib/libnfc.so.5 nfc-list
+
 /usr/lib/ESZEMELYI/eszig-cmu
