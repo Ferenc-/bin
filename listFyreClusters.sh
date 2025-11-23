c6ae01c5969d6bde15be00e6c374ed50040ffa09
Ferenc Géczi <ferenc.gm@gmail.com>
2025-11-23 00:00:00 +0000

    Adapt listFyreClusters.sh to new ocpapi
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- listFyreClusters.sh
+++ listFyreClusters.sh
@@ -3,4 +3,4 @@
 # USERNAMES ARE WITHOUT AN '@' !
 USERNAME=$(yq ".username" ~/.fyre.yaml)
 API_KEY=$(yq ".apikey" ~/.fyre.yaml)
-curl -X GET -k -u "${USERNAME}:${API_KEY}" 'https://api.fyre.ibm.com/rest/v1/?operation=query&request=showclusters' | jq 
+curl -s -X GET -k -u "${USERNAME}:${API_KEY}" 'https://ocpapi.svl.ibm.com/v1/clusters/' | jq
