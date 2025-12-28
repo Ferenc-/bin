3d74241a3f1dc0367c484df144156d304ea2f5ac
Ferenc Géczi <ferenc.gm@gmail.com>
2025-12-28 00:00:00 +0000

    fix: Make filtering case insensitive in instanaGrepAllK8sensorLogs
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- instanaGrepAllK8sensorLogs.sh
+++ instanaGrepAllK8sensorLogs.sh
@@ -2,4 +2,4 @@
 
 PATTERN="${1:-app=k8sensor@}"
 
-for po in $(kubectl get -n instana-agent po -l app=k8sensor -o name | cut -f2 -d '/'); do kubectl logs -n instana-agent "${po}" | grep "${PATTERN}" ; done
+for po in $(kubectl get -n instana-agent po -l app=k8sensor -o name | cut -f2 -d '/'); do kubectl logs -n instana-agent "${po}" | grep -i "${PATTERN}" ; done
