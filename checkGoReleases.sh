baf18a7f2c4c3835ab23583bf753a84cb72c3057
Ferenc Géczi <ferenc.gm@gmail.com>
2026-02-07 00:00:00 +0000

    Drop checkGoReleases.sh
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- checkGoReleases.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CURRENT=$(go version | awk '{print $3}' | sed 's/go//')
-LATEST=$(curl -s https://go.dev/VERSION?m=text | head -1 | sed 's/go//')
-
-if [ "${CURRENT}" != "${LATEST}" ]; then
-    echo -e "\033[31mNew Go version available: ${LATEST} (current: ${CURRENT})\033[0m"
-else
-    echo -e "\033[32mGo version is already the latest ${LATEST}\033[0m"
-fi
