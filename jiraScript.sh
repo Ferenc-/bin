c928c77bb81b27afd20278da9be2ef5e21757fef
Ferenc Géczi <ferenc.gm@gmail.com>
2026-02-17 00:00:00 +0000

    Add jiraScript.sh
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

---
+++ jiraScript.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+BEARER_TOKEN=$(<~/.private/JIRA_TOKEN)
+
+JQL_QUERY='assignee=currentUser()+AND+status+IN+("QA",+"In+Development")'
+JQ_FILTER='.issues[] | .fields.status.name, .key, .fields.summary'
+
+curl X GET \
+  --silent \
+  -H "Authorization: Bearer ${BEARER_TOKEN}" \
+  -H "Content-Type: application/json" \
+  "https://jsw.ibm.com/rest/api/latest/search?jql=${JQL_QUERY}" \
+| jq --color-output "${JQ_FILTER}" \
+| tr '\n' ' ' \
+| xargs printf '%-26s | %s | %s\n' \
+| sort --reverse
