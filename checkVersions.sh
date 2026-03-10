6397fd2c9496d2a827afad4de6bd2410175fdced
Ferenc Géczi <ferenc.gm@gmail.com>
2026-03-10 00:00:00 +0000

    The pushd is not available on ash (OpenWrt)
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- checkVersions.sh
+++ checkVersions.sh
@@ -38,7 +38,7 @@ kubernetes() {
 
 openwrt() {
   NAME='OpenWrt'
-  CURRENT='25.12.0'
+  CURRENT='25.12.1'
   LATEST="$(wget -qO- https://downloads.openwrt.org/.versions.json | awk -F '"' '/"stable_version"/{print $4}')"
 }
 
