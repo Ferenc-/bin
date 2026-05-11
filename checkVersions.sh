a8063f310f07ef985f5d6306bb72b66430f495d3
Ferenc Géczi <ferenc.gm@gmail.com>
2026-05-11 00:00:00 +0000

    Move to OpenWrt 25.12.4
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- checkVersions.sh
+++ checkVersions.sh
@@ -38,7 +38,7 @@ kubernetes() {
 
 openwrt() {
   NAME='OpenWrt'
-  CURRENT='25.12.3'
+  CURRENT='25.12.4'
   LATEST="$(wget -qO- https://downloads.openwrt.org/.versions.json | awk -F '"' '/"stable_version"/{print $4}')"
 }
 
