7e7db18586c388a2097242837698b56d712bf146
Ferenc Géczi <ferenc.gm@gmail.com>
2026-09-11 00:00:00 +0000

    Fix `checkVersions.sh`
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- checkVersions.sh
+++ checkVersions.sh
@@ -54,13 +54,7 @@ postmarketos() {
   LATEST="$(wget -qO- 'https://gitlab.postmarketos.org/postmarketOS/pmaports/-/raw/master/channels.cfg?ref_type=heads&inline=false' | awk 'match($0, /[[]v([0-9]{2}[.][0-9]{2})[]]/, arr) {printf "%s", arr[1]; exit}')"
 }
 
-<<<<<<< Updated upstream
-for i in freedesktopsdk golang kubernetes openwrt postmarketos; do
-||||||| Stash base
-for i in freedesktopsdk golang kubernetes openwrt postamrketos; do
-=======
-for i in flatpakKdePlatform freedesktopsdk golang kubernetes openwrt postamrketos; do
->>>>>>> Stashed changes
+for i in flatpakKdePlatform freedesktopsdk golang kubernetes openwrt postmarketos; do
   ${i}
   check
 done
